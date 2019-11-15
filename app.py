cf69c536061e80f2cf9e3d29da50a0e18c7b007d
Gustavosmc <luizgustavosmc@gmail.com>
2019-11-15 07:42:50 -0300

    ...

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from sql_to_json import get_tables, command_generate
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def uploader_file():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
